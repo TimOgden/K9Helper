dfc3f90309cc7ff36b57a4d41048630b188f5882
Tim Ogden <togden1999@gmail.com>
2019-01-14 12:18:06 -0500

    Functional

--- windowGetter.py
+++ windowGetter.py
@@ -2,12 +2,14 @@ from win32gui import GetWindowText, GetForegroundWindow
 import time
 import datetime
 from faceDetector import hasFace, detect, close
-check_freq_sec = 10
+check_freq_sec = 30
 applications_time = {}
-num_hours = 0.08333333333 # 5 mins
+num_hours = 12
 t_end = time.time() + num_hours * 60 * 60
-
+start_time = 0
+end_time = 0
 def main():
+	start_time = datetime.datetime.now()
 	while time.time() < t_end:
 		if hasFace():
 			log_window()
@@ -16,6 +18,7 @@ def main():
 		else:
 			print('No face found')
 		time.sleep(check_freq_sec)
+	end_time = datetime.datetime.now()
 	publish_log()
 	close()
 	print('done')
@@ -32,8 +35,10 @@ def publish_log():
 	now = datetime.datetime.now()
 	with open('windowslog-{}-{}-{}.txt'.format(now.month,now.day,now.year), 'w') as f:
 		val = ""
+		val += start_time + '\n'
 		for key in applications_time:
 			val += key + "\n" + str(applications_time[key]) + "\n"
+		val += end_time + '\n'
 		f.write(val)
 
 def window():
